ccb85d20832c709fefbb833b66c40275df3dff0e
Timothy Herold <heroldt@gmail.com>
2016-10-28 09:11:31 -0700

    Show update-answer component when edit button is clicked on an answer.

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   showAddAnswer: false,
+  answerToUpdate: null,
   actions: {
+    showUpdate(answer) {
+      this.set('answerToUpdate', answer.get('id'));
+    },
+    cancelUpdate() {
+      this.set('answerToUpdate', null);
+    },
     enableAddAnswer() {
       this.set('showAddAnswer', true);
     },
