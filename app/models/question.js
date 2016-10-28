aade1173d996c26b59af86539fc9c5051a2044f5
Timothy Herold <heroldt@gmail.com>
2016-10-27 21:42:55 -0700

    Add ability to display individual questions.

--- app/models/question.js
+++ app/models/question.js
@@ -1,12 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+import moment from 'moment';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string'),
   author: DS.attr('string'),
-  timestamp: DS.attr('number', { defaultValue() {
-    var now = new Date();
-    return now.getTime();
-    }
+  timestamp: DS.attr('number', { defaultValue() { return moment(); }}),
+
+  date: Ember.computed('timestamp', function() {
+    var now = moment();
+    var then = this.get('timestamp');
+    return `${moment.duration(now - then, "milliseconds").humanize()} ago`;
   })
 });
