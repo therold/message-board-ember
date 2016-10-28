69a988ac2ed85949ea0a43319cb21d956d84beea
Timothy Herold <heroldt@gmail.com>
2016-10-27 20:47:09 -0700

    Display questions on home page.

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('new-question');
+  this.route('question', {path: '/question/:question_id'});
 });
 
 export default Router;
