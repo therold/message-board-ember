e5b3b55e079e835fc740f6041ce1c62513baa41c
Timothy Herold <heroldt@gmail.com>
2016-10-29 12:57:27 -0700

    Add display of top 5 tags on tags route.

--- app/models/tag.js
+++ app/models/tag.js
@@ -1,6 +1,11 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
-    questions: DS.hasMany('question', { async: true })
+    questions: DS.hasMany('question', { async: true }),
+
+    questionCount: Ember.computed('questions', function() {
+      return this.get('questions').get('length');
+    })
 });
--- app/routes/tag.js
+++ app/routes/tag.js
@@ -5,7 +5,9 @@ export default Ember.Route.extend({
     return Ember.RSVP.hash({
       name: params.tag_name,
       tag: this.store.query('tag', { orderBy: 'name', equalTo: params.tag_name})
-      .then(result => { return result.objectAt(0); })
+      .then(result => { return result.objectAt(0); }),
+      topTags: this.store.findAll('tag')
+        .then(tag => tag.sortBy('questionCount').reverse().objectsAt([0,1,2,3,4]))
     });
   },
 
