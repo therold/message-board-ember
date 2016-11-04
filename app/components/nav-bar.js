443ee902ad96f538e84c7c9e6fda0784010604e4
Timothy Herold <heroldt@gmail.com>
2016-11-03 20:45:36 -0700

    Add UI for logging out on nav-bar.

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -2,4 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   userService: Ember.inject.service(),
+
+  actions: {
+    logout() {
+      this.get('userService').logout();
+    }
+  }
+
 });
