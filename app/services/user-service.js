9e2a7056cdc8e280e20edad489045ff72e409903
Timothy Herold <heroldt@gmail.com>
2016-11-03 20:42:56 -0700

    Add function to support logging out.

--- app/services/user-service.js
+++ app/services/user-service.js
@@ -5,27 +5,31 @@ export default Ember.Service.extend({
   currentUser: null,
 
   add(name, password) {
-      var service = this;
-      var firebase = service.get('firebase');
-      var params = { name: name, password: password };
-      var path = firebase.child('users');
-      return new Ember.RSVP.Promise(function(resolve, reject) {
-        path.orderByChild('name').equalTo(name).limitToFirst(1).once('value').then(user => {
-          if(user.val()) {
-            reject(Error('UserExistsError'));
-          } else {
-            path.push(params).then(user => {
-              var id = user.getKey();
-              path.child(id).update({id: id}).then(() => {
-                user.once('value').then(user => {
-                  service.set('currentUser', user.val());
-                  resolve();
-                });
+    var service = this;
+    var firebase = service.get('firebase');
+    var params = { name: name, password: password };
+    var path = firebase.child('users');
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      path.orderByChild('name').equalTo(name).limitToFirst(1).once('value').then(user => {
+        if(user.val()) {
+          reject(Error('UserExistsError'));
+        } else {
+          path.push(params).then(user => {
+            var id = user.getKey();
+            path.child(id).update({id: id}).then(() => {
+              user.once('value').then(user => {
+                service.set('currentUser', user.val());
+                resolve();
               });
             });
-          }
-        });
+          });
+        }
       });
-    },
+    });
+  },
+
+  logout() {
+    this.set('currentUser', null);
+  },
 
 });
