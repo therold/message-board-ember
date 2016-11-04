0f11c967308b226ac5efbdea2be69f486de6d322
Timothy Herold <heroldt@gmail.com>
2016-11-03 19:57:13 -0700

    Add ability to add users in user-service.

--- app/services/user-service.js
+++ app/services/user-service.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  firebase: Ember.inject.service(),
+  currentUser: null,
+
+  add(name, password) {
+      var service = this;
+      var firebase = service.get('firebase');
+      var params = { name: name, password: password };
+      var path = firebase.child('users');
+      return new Ember.RSVP.Promise(function(resolve, reject) {
+        path.orderByChild('name').equalTo(name).limitToFirst(1).once('value').then(user => {
+          if(user.val()) {
+            reject(Error('UserExistsError'));
+          } else {
+            path.push(params).then(user => {
+              var id = user.getKey();
+              path.child(id).update({id: id}).then(user => {
+                service.set('currentUser', user);
+                resolve();
+              });
+            });
+          }
+        });
+      });
+    },
+
 });
