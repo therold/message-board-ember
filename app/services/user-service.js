34b61e8d60f26c15bbd3363819e0611ee72adf34
Timothy Herold <heroldt@gmail.com>
2016-11-03 21:12:14 -0700

    Add abilty to log in to existing account in user-service.

--- app/services/user-service.js
+++ app/services/user-service.js
@@ -28,8 +28,30 @@ export default Ember.Service.extend({
     });
   },
 
-  logout() {
-    this.set('currentUser', null);
+  login(name, password) {
+    var service = this;
+    var firebase = service.get('firebase');
+    var params = { name: name, password: password };
+    var path = firebase.child('users');
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      path.orderByChild('name').equalTo(name).limitToFirst(1).once('value').then(data => {
+        var user = data.val()
+        if(!user) {
+          reject(Error('UserInvalidError'));
+        } else {
+          var user_id = Object.keys(user)[0];
+          if(params.password !== user[user_id].password) {
+            reject(Error('UserInvalidError'));
+          } else {
+            service.set('currentUser', user[user_id]);
+            resolve();
+          }
+        }
+      });
+    });
   },
 
+  logout() {
+    this.set('currentUser', null);
+  }
 });
