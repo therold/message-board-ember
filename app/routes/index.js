18ebfda362bca350e4995db7ff32f7906d53d896
Timothy Herold <heroldt@gmail.com>
2016-11-03 22:18:03 -0700

    port index to question service.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  questionService: Ember.inject.service(),
+
   model() {
     return Ember.RSVP.hash({
-      question: this.store.findAll('question', { reload: true }).then(question => {
-        return question.sortBy('timestamp').reverse();
-      }),
+      question: this.get('questionService').all(),
       topTags: this.store.findAll('tag')
         .then(tag => tag.sortBy('questionCount').reverse().objectsAt([0,1,2,3,4]))
     });
