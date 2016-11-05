7d7f144f8b5672dec405fedb0460df29541d6a26
Timothy Herold <heroldt@gmail.com>
2016-11-05 10:35:42 -0700

    Refactor answer-service's all() method to use DS.store.

--- app/services/answer-service.js
+++ app/services/answer-service.js
@@ -2,36 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   firebase: Ember.inject.service(),
-  questionService: Ember.inject.service(),
   store: Ember.inject.service(),
+  questionService: Ember.inject.service(),
+
+  all() {
+    var store = this.get('store');
+    return store.findAll('question');
+  },
 
   findByQuestionId(question_id) {
     var store = this.get('store');
     var questionService = this.get('questionService');
-    var output = [];
     var promises = [];
+    var answers = [];
 
     return questionService.getAnswerIds(question_id).then(answer_ids => {
       answer_ids.forEach(answer_id => {
         promises.push(
-          store.find('answer', answer_id).then(answer => { output.push(answer); })
+          store.find('answer', answer_id).then(answer => { answers.push(answer); })
         );
       });
-      return Ember.RSVP.all(promises).then(() => { return output; });
+      return Ember.RSVP.all(promises).then(() => { return answers; });
     });
   },
 
-  all() {
-    var service = this;
-    var store = service.get('store');
-    var firebase = service.get('firebase');
-    var output = [];
-    return firebase.child('answers').once('value').then(data => {
-      data.forEach(answer => {
-        var answerRecord = store.createRecord('answer', answer.val());
-        output.push(answerRecord);
-      });
-      return output;
-    });
-  }
 });
